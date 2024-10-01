c17d804e66ca2625cb525db36196eadf0124eb6e
unknown <kaito.stenroos@outlook.com>
2024-10-01 15:07:15 +0300

    First commit

---
+++ src/main/java/com/kaitostenroos/detailedtraintimetable/detailedtraintimetable/DetailedtraintimetableApplication.java
@@ -0,0 +1,13 @@
+package com.kaitostenroos.detailedtraintimetable.detailedtraintimetable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetailedtraintimetableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DetailedtraintimetableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kaitostenroos/detailedtraintimetable/detailedtraintimetable/DetailedtraintimetableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kaitostenroos.detailedtraintimetable.detailedtraintimetable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DetailedtraintimetableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
